53d16dd815096b430b61d4aac23cb97ab00655b4
zwolinskirj <zwolinskirj@sunybroome.edu>
2025-03-13 09:57:07 -0400

    Updated to contain delete double

--- LL_Demo.cpp
+++ LL_Demo.cpp
@@ -4,14 +4,14 @@
 using namespace std;
 
 struct dbl_node {
-    string word;
-    dbl_node* prev;
-    dbl_node* next;
+	string word;
+	dbl_node* prev;
+	dbl_node* next;
 };
 
 struct sngl_node {
-    string word;
-    sngl_node* next;
+	string word;
+	sngl_node* next;
 };
 
 
@@ -25,143 +25,185 @@ dbl_node* dbl_tail = nullptr;
 // Insert at beginning of a DLL
 void insertDouble(const string& w)
 {
-    dbl_node* temp = new dbl_node;
-    if (dbl_head == nullptr)
-    {
-        temp->word = w;
-        temp->next = nullptr;
-        temp->prev = nullptr;
-        dbl_head = temp; // Update the front of the list
-        dbl_tail = temp;
-    }
-    else
-    {
-        temp->word = w;
-        temp->next = dbl_head; // Point to the front of the list
-        temp->prev = nullptr;
-        dbl_head->prev = temp;
-        dbl_head = temp; // Update the front of the list
-    }
+	dbl_node* temp = new dbl_node;
+	if (dbl_head == nullptr)
+	{
+		temp->word = w;
+		temp->next = nullptr;
+		temp->prev = nullptr;
+		dbl_head = temp; // Update the front of the list
+		dbl_tail = temp;
+	}
+	else
+	{
+		temp->word = w;
+		temp->next = dbl_head; // Point to the front of the list
+		temp->prev = nullptr;
+		dbl_head->prev = temp;
+		dbl_head = temp; // Update the front of the list
+	}
 }
 
 // Insert at beginning of a SLL
-void insertSingle(const string &w) {
-    sngl_node* temp = new sngl_node;
+void insertSingle(const string& w) {
+	sngl_node* temp = new sngl_node;
 
-    if (sngl_head == nullptr) {
-        sngl_tail = temp;
-    }
-    temp->word = w;
-    temp->next = sngl_head; // Point to the front of the list
-    sngl_head = temp; // Update the front of the list
+	if (sngl_head == nullptr) {
+		sngl_tail = temp;
+	}
+	temp->word = w;
+	temp->next = sngl_head; // Point to the front of the list
+	sngl_head = temp; // Update the front of the list
 
 }
 
 // Insert at the end of a SLL with tail
 void insertEndSingle(const string& w) {
-    sngl_node* temp = new sngl_node;
-    temp->word = w;
-    temp->next = nullptr;
-    sngl_tail->next = temp;
-    sngl_tail = temp;
+	sngl_node* temp = new sngl_node;
+	temp->word = w;
+	temp->next = nullptr;
+	sngl_tail->next = temp;
+	sngl_tail = temp;
 }
 
 // Insert at the end of a DLL with tail
 void insertEndDouble(const string& w) {
-    dbl_node* temp = new dbl_node;
-    temp->word = w;
-    temp->next = nullptr;
-    dbl_tail->next = temp;
-    temp->prev = dbl_tail;
-    dbl_tail = temp;
+	dbl_node* temp = new dbl_node;
+	temp->word = w;
+	temp->next = nullptr;
+	dbl_tail->next = temp;
+	temp->prev = dbl_tail;
+	dbl_tail = temp;
 }
 
 void deleteSingle(const string& w) {
-    sngl_node* curr = new sngl_node;
-    sngl_node* prev = nullptr;
-    if (sngl_head != nullptr) {
-        curr = sngl_head;
-    }
-    while (curr != nullptr && curr->word != w) {
-        prev = curr;
-        curr = curr->next;
-    }
-    if (curr == nullptr)
-        cout << w << " is not present in the list." << endl;
-    else if (curr == sngl_head){
-        sngl_head = curr->next;
-        delete curr;
-    }
-    else {
-        prev->next = curr->next;
-        if (curr == sngl_tail)
-            sngl_tail = prev;
-        delete curr;
-    }
+	sngl_node* curr = new sngl_node;
+	sngl_node* prev = nullptr;
+	if (sngl_head != nullptr) {
+		curr = sngl_head;
+	}
+	while (curr != nullptr && curr->word != w) {
+		prev = curr;
+		curr = curr->next;
+	}
+	if (curr == nullptr)
+		cout << w << " is not present in the list." << endl;
+	else if (curr == sngl_head) {
+		sngl_head = curr->next;
+		delete curr;
+	}
+	else {
+		prev->next = curr->next;
+		if (curr == sngl_tail)
+			sngl_tail = prev;
+		delete curr;
+	}
+}
+
+void deleteDouble(const string& w) {
+	dbl_node* curr = new dbl_node;
+	dbl_node* prev = nullptr;
+	if (dbl_head != nullptr) {
+		curr = dbl_head;
+	}
+	while (curr != nullptr && curr->word != w) {
+		prev = curr;
+		curr = curr->next;
+	}
+	if (curr == nullptr)
+		cout << w << " is not present in the list." << endl;
+	else if (curr->prev != NULL)
+		curr->prev->next = curr->next;
+
+	// Update the next node's prev pointer
+	if (curr->next != NULL)
+		curr->next->prev = curr->prev;
+
+	// If the node to be deleted is the head node
+	if (dbl_head == curr)
+		dbl_head = curr->next;
+
+	// Deallocate memory for the deleted node
+	delete curr;
+
+
+	/*if (curr == dbl_head) {
+	dbl_head = curr->next;
+	curr->next =
+	delete curr;
+}
+else {
+	prev->next = curr->next;
+	if (curr == sngl_tail)
+		sngl_tail = prev;
+	delete curr;
+}*/
 }
 
 // Print the SLL
 void displaySingle()
 {
-    cout << "The list: ";
-    sngl_node* temp = sngl_head;
-    while (temp != nullptr)
-    {
-        cout << temp->word << " ";
-        temp = temp->next;
-    }
-    cout << endl;
+	cout << "The list: ";
+	sngl_node* temp = sngl_head;
+	while (temp != nullptr)
+	{
+		cout << temp->word << " ";
+		temp = temp->next;
+	}
+	cout << endl;
 }
 
 // Print the DLL
 void displayDouble()
 {
-    cout << "The list: ";
-    dbl_node* temp = dbl_head;
-    while (temp != nullptr)
-    {
-        cout << temp->word << " ";
-        temp = temp->next;
-    }
-    cout << endl;
+	cout << "The list: ";
+	dbl_node* temp = dbl_head;
+	while (temp != nullptr)
+	{
+		cout << temp->word << " ";
+		temp = temp->next;
+	}
+	cout << endl;
 }
 
 void displayHead() {
-  cout << "The head: ";
-  cout << sngl_head->word << "\n";
+	cout << "The head: ";
+	cout << sngl_head->word << "\n";
 }
 
 void displayTail() {
-  cout << "The tail: ";
-  cout << sngl_tail->word << "\n";
+	cout << "The tail: ";
+	cout << sngl_tail->word << "\n";
 }
 
 int main() {
-    insertSingle("Fred");
-    displayHead();
-    displayTail();
-    insertSingle("Mike");
-    displayHead();
-    displayTail();
-    insertSingle("Sarah");
-    displayHead();
-    displayTail();
-    insertEndSingle("Kyle");
-    displayHead();
-    displayTail();
-    displaySingle();
-    deleteSingle("Sarah");
-    displaySingle();
-    deleteSingle("Fred");
-    displaySingle();
-    deleteSingle("Kyle");
-    displaySingle();
-    cout << endl;
+	insertSingle("Fred");
+	displayHead();
+	displayTail();
+	insertSingle("Mike");
+	displayHead();
+	displayTail();
+	insertSingle("Sarah");
+	displayHead();
+	displayTail();
+	insertEndSingle("Kyle");
+	displayHead();
+	displayTail();
+	displaySingle();
+	deleteSingle("Sarah");
+	displaySingle();
+	deleteSingle("Fred");
+	displaySingle();
+	deleteSingle("Kyle");
+	displaySingle();
+	cout << endl;
 
-    insertDouble("Erin");
-    insertDouble("Kate");
-    insertDouble("Nadine");
-    insertEndDouble("Ann");
-    displayDouble();
-    return 0;
+	insertDouble("Erin");
+	insertDouble("Kate");
+	insertDouble("Nadine");
+	insertEndDouble("Ann");
+	displayDouble();
+	deleteDouble("Kate");
+	displayDouble();
+	return 0;
 }
